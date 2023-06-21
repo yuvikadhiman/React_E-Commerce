bf4eec9373e4c8938d112b81dba37f98d5c91a4b
yuvikadhiman <98342858+yuvikadhiman@users.noreply.github.com>
2023-06-21 21:48:47 +0530

    .

---
+++ e-commerce/src/context/ProductsContext.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useContext, useEffect, useReducer,createContext } from 'react'
+const ProductsContextProvider = () => {
+
+  const ProductContext=createContext();
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductsContextProvider
--- e-commerce/src/pages/SharedLayout.js
+++ e-commerce/src/pages/SharedLayout.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import SideBar from '../components/SideBar'
 const SharedLayout = () => {
   return (
     <>
     <Navbar/>
+    <SideBar/>
       <Outlet/>
     <Footer/>
-    
     </>
   )
 }
